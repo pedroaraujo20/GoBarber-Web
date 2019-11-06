003fbd3ce0dc324a2782fc65cb9a6e2edf3b0a11
Pedro Araujo <pedro-foa@hotmail.com>
2019-11-05 22:17:50 -0300

    styled signin and signup

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import logo from '~/assets/gobarber.svg';
 
 // import { Container } from './styles';
 
 export default function SignIn() {
-  return <h1>Login</h1>;
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+
+      <form>
+        <input type="email" placeholder="Seu e-amail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Acessar</button>
+        <Link to="/register">Criar conta gratuita</Link>
+      </form>
+    </>
+  );
 }
